7d097a161903b9eecd5b6e2514a69e4874b8ee9a
JorgeMz <jorge.martinez09@uptc.edu.co>
2023-05-21 17:18:30 -0500

    Mixer jejeje

--- shooter.py
+++ shooter.py
@@ -43,9 +43,9 @@ grenade_thrown = False
 
 
 # load music and sounds
-# pygame.mixer.music.load('audio/music2.mp3')
-# pygame.mixer.music.set_volume(0.3)
-# pygame.mixer.music.play(-1, 0.0, 5000)
+pygame.mixer.music.load('audio/music2.mp3')
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(-1, 0.0, 5000)
 jump_fx = pygame.mixer.Sound("audio/jump.wav")
 jump_fx.set_volume(0.05)
 shot_fx = pygame.mixer.Sound("audio/shot.wav")
