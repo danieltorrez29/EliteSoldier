73c993172a47a463cc70d7ae26401c60908d3e18
danieltorrez29 <daniel.torres04@uptc.edu.co>
2023-05-22 22:56:39 -0500

    Elite Soldier - Window Title

--- shooter.py
+++ shooter.py
@@ -20,7 +20,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Shooter")
+pygame.display.set_caption("Elite Soldier")
 
 # set framerate
 clock = pygame.time.Clock()
